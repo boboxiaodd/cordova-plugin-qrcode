97aedbd52d5d34cfc9166e5249e3dfb07a4896c4
boboxiaodd <iam386@gmail.com>
2024-02-21 15:36:49 +0800

    Update qrcode.js

--- www/qrcode.js
+++ www/qrcode.js
@@ -1,7 +1,7 @@
 const exec = require('cordova/exec');
 const CDVQRCode = {
-    scan_qrcode:function (success){
-        exec(success,null,'CDVQRCode','scan_qrcode',[]);
+    scan_qrcode:function (option,success){
+        exec(success,null,'CDVQRCode','scan_qrcode',[option]);
     }
 };
 module.exports = CDVQRCode;
